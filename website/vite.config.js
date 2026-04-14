beb5180e2bb584c9bc51677f3138e65b814bd1a3
Oguzhan Akdogan <oguzakdogaan@gmail.com>
2026-04-14 23:38:23 +0300

    chore: set base path to root for AdMob compatibility

--- website/vite.config.js
+++ website/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/astroterm.github.io-/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
